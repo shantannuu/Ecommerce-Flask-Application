bf9c01b2250ec9d181b9eca4a260e68e8d31a910
shantannuu <shantanubolate@gmail.com>
2024-05-19 10:07:54 +0530

    shan

--- ecommerce-backend/venv/Scripts/activate.bat
+++ ecommerce-backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Ecommerce-Flask-Application\ecommerce-backend\venv
+set VIRTUAL_ENV=C:\Users\spatil\Documents\Ecommerce-Flask-Application\ecommerce-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
